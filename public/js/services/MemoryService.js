eefccc5c2123d4a3f8e22b075a315c688b86d2e1
dreddy79 <devendren.reddy@gmail.com>
2015-01-16 11:06:19 -0600

    start planning out memory service work

--- public/js/services/MemoryService.js
+++ public/js/services/MemoryService.js
@@ -1,12 +1,38 @@
 angular.module('MemoryService', []).factory('Memory', ['$http', function($http) {
         
-        function Card() {
+        var self = this;
+        
+        self.cards = [];
+
+        function Card(picIndex) {
             var flipped = false;
-            var srcImg = 0;
+            var srcImg = picIndex;
         };    
 
-        var initialize = function (numberOfcards){
-            
+        function initialize(numberOfCards) {
+            if (numberOfCards % 2 !== 0) {
+                throw "Invalid number of initialization cards.  Count must be even.";
+            } 
+            else {
+                for (var i = 0; i < numberOfCards/2 - 1 ; i++) {
+                    self.cards.push(new Card(i));
+                }
+
+                for (var i = numberOfCards / 2; i < numberOfCards - 1 ; i++) {
+                    self.cards.push(new Card(i));
+                }
+
+            }
+        };
+        
+        function cardFlip(card){
+
         }
+
+        return {
+            cards: self.cards,
+            initialize: initialize,
+            cardFlip: cardFlip
+        };
 
 }]);
