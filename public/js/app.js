d6bfae937480fada4bc8a82af3a70ddc10438e15
dreddy79 <devendren.reddy@gmail.com>
2015-01-18 10:13:08 -0600

    Update to main navigation

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 angular.module('learned', ['ngRoute', 
-                           'appRoutes', 
+                           'appRoutes',
 						   'Main', 
                            'MemoryService',
                            'AdditionService',
