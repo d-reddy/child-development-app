c18c2455ad07ea45652f34d62969759a33ddc442
dreddy79 <devendren.reddy@gmail.com>
2015-01-19 19:44:08 -0600

    try adding support for fastclick to stop delay on touch on ipad

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -2,4 +2,8 @@ angular.module('Main', []).controller('MainController', function ($scope) {
     document.ontouchmove = function (event) {
         event.preventDefault();
     }
+
+    $(function () {
+        FastClick.attach(document.body);
+    });
 });
