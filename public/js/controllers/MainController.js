664af6cc1df011b77594bf27367b4e12002db60f
dreddy79 <devendren.reddy@gmail.com>
2015-01-31 14:12:08 -0600

    Try to lock in landscape mode

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -7,6 +7,15 @@ angular.module('Main', []).controller('MainController', function ($scope) {
         FastClick.attach(document.body);
     });
 
+    $(function () {
+        function reorient(e) {
+            var portrait = (window.orientation % 180 == 0);
+            $("body > div").css("-webkit-transform", portrait ? "rotate(-90deg)" : "");
+        }
+        window.onorientationchange = reorient;
+        window.setTimeout(reorient, 0);
+    });
+
     //var iframe = document.createElement('iframe');
     //iframe.style.display = "none";
     //iframe.src = 'http://www.elearnenglishlanguage.com/sounds/a.wav';
